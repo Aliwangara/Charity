b94dc8c90dc4a62d636fc7b13d06c9086c3dc99d
Aliwangara <aliwangara63@gmail.com>
2024-06-29 23:26:59 +0300

    update database settings settings
    I cleared some lines on database settings to enhance efficiency

--- Charity/Spreading/Spreading/settings.py
+++ Charity/Spreading/Spreading/settings.py
@@ -97,13 +97,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'CLIENT': {
-            'host': 'mongodb+srv://aliwangara63:l1FWqVWW0qBVoIFx@cluster0.uthzjek.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', # Replace with your MongoDB server address
-            'port': 27017,               # Replace with your MongoDB port if different
-            'username': 'ali wangara', # Replace with your MongoDB username (optional)
+            'host': 'mongodb+srv://aliwangara63:l1FWqVWW0qBVoIFx@cluster0.pbhumyz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', # Replace with your MongoDB server address
+            # 'port': 27017,               # Replace with your MongoDB port if different
+            'username': 'aliwangara63', # Replace with your MongoDB username (optional)
             'password': 'l1FWqVWW0qBVoIFx', # Replace with your MongoDB password (optional)
-            'authSource': 'your_auth_database', # Replace with your MongoDB authentication database (optional)
+             
         },
-        'NAME': 'Charity',
+        'NAME': 'Charity'
+
+,
     }
 }
 
