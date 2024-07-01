27f81fee148575bdc912c2ae060dcd7a749ce633
Aliwangara <aliwangara63@gmail.com>
2024-07-01 19:50:52 +0300

    Update settings.py
    reverted back to sqlite database for changes so that it doesnt interfere with the data I was having

--- Charity/Spreading/Spreading/settings.py
+++ Charity/Spreading/Spreading/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-import pymongo
+# import pymongo
 from django.contrib import messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,32 +82,30 @@ WSGI_APPLICATION = 'Spreading.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'CLIENT': {
-            'host': 'mongodb+srv://aliwangara63:l1FWqVWW0qBVoIFx@cluster0.pbhumyz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', # Replace with your MongoDB server address
-            # 'port': 27017,               # Replace with your MongoDB port if different
-            'username': 'aliwangara63', # Replace with your MongoDB username (optional)
-            'password': 'l1FWqVWW0qBVoIFx', # Replace with your MongoDB password (optional)
-             
-        },
-        'NAME': 'Charity'
-
-,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'djongo',
+#         'CLIENT': {
+#             'host': 'mongodb+srv://aliwangara63:l1FWqVWW0qBVoIFx@cluster0.pbhumyz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', # Replace with your MongoDB server address
+#             # 'port': 27017,               # Replace with your MongoDB port if different
+#             'username': 'aliwangara63', # Replace with your MongoDB username (optional)
+#             'password': 'l1FWqVWW0qBVoIFx', # Replace with your MongoDB password (optional)
+             
+#         },
+#         'NAME': 'Charity',
+#     }
+# }
 
 
 
