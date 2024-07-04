1ef1e9b1b66f7e1996d225799d02c92f350d4940
Aliwangara <aliwangara63@gmail.com>
2024-07-05 01:28:50 +0300

    update credit card.html
    I added svg to visualise credit cards

--- Charity/Spreading/Spreading/settings.py
+++ Charity/Spreading/Spreading/settings.py
@@ -172,7 +172,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'main_app/assets')
