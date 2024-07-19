fe57153eb19ca3a1372a4e600684d05b061d31b6
Aliwangara <aliwangara63@gmail.com>
2024-07-20 02:14:47 +0300

    Update urls.py

--- Charity/Spreading/Spreading/urls.py
+++ Charity/Spreading/Spreading/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-from . import settings
+from Spreading.settings import base
+from Spreading import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('main_app.urls')),
     path('',include('Payment.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(base.MEDIA_URL, document_root=base.MEDIA_ROOT)
