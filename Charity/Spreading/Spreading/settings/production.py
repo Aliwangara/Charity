6391bf2f5e03aa3c0dfb16e72aefd77889dede26
Aliwangara <aliwangara63@gmail.com>
2024-08-07 10:40:47 +0300

    Update production.py

--- Charity/Spreading/Spreading/settings/production.py
+++ Charity/Spreading/Spreading/settings/production.py
@@ -1,3 +1,21 @@
-from .base import*
+from .base import *
+import os
+from dotenv import load_dotenv
+import dj_database_url
+
+load_dotenv()
+
 
 ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': 'railway',
+                'USER': 'postgres',
+                'PASSWORD': os.environ.get('DB_PASSWORD'),
+                'HOST': 'postgres.railway.internal',
+                'PORT': '5432',
+                'Default': dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=1800)
+    }
+}
