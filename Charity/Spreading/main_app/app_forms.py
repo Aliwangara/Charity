97f4ecdf2a3a2384e7bddfa1283ec6f07b55d917
Aliwangara <aliwangara63@gmail.com>
2024-08-07 10:38:21 +0300

    Update app_forms.py
    Added a happy cutomers form for happy model and number form for numbr mode

--- Charity/Spreading/main_app/app_forms.py
+++ Charity/Spreading/main_app/app_forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from main_app.models import Volunteers, cause, Event
+from main_app.models import Volunteers, cause, Event, happy_customers, Number
 
 
 class Volunteer_form(forms.ModelForm):
@@ -32,4 +32,17 @@ class Event_forms(forms.ModelForm):
     class Meta:
         model = Event
         fields = ['name', 'profile', 'About', 'subject', 'is_new', 'is_superuser']
-       
+        
+
+class Happy_customers_form(forms.ModelForm):
+    
+    class Meta:
+        model = happy_customers
+        fields ="__all__"
+        
+        
+class Number_form(forms.ModelForm):
+    
+    class Meta:
+        model = Number
+        fields = ['name','number']
