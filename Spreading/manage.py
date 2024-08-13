dea3d6c0697ad98928881b97017d3c6f691a8f46
Aliwangara <134551530+Aliwangara@users.noreply.github.com>
2024-08-13 20:04:36 +0300

    Update manage.py

--- Spreading/manage.py
+++ Spreading/manage.py
@@ -6,18 +6,8 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Spreading.settings')
-    # if base.DEBUG:
-    #     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Spreading.settings.local')
-    # else:
-    #     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Spreading.settings.production')
-    
-    
-    # env = os.getenv('DJANGO_ENVIRONMENT', 'local')
-    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'Spreading.settings.{env}')
-        
+    """Run administrative tasks."""  
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Spreading.settings')    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
